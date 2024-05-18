7f2813468e85a9a3a739dc9a2b7c957b146bbc17
Kavin Muralikrishnan <142550397+Advay17@users.noreply.github.com>
2024-05-18 15:48:19 -0400

    clean up

--- Room Items/Clean Up/clean_up.gd
+++ Room Items/Clean Up/clean_up.gd
@@ -1,25 +1,66 @@
 extends Area2D
-var tex
+@export var texture:Texture2D
 @onready var sprite:Sprite2D=$Sprite2D
-
+var cleaning:=false
+@onready var timer:Timer=$Timer
+var tween:Tween
+var tween2:Tween
+var color
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var name:="CollisionPolygon2D"
 	remove_child($CollisionPolygon2D)
 	for i in range(2, 9):
 		remove_child(get_node(str(name, i)))
-	var texture:=sprite.texture.get_image()
+	sprite.texture=texture
+	var image:=sprite.texture.get_image()
 	var bitmap:=BitMap.new()
-	bitmap.create_from_image_alpha(texture)
+	bitmap.create_from_image_alpha(image)
 	var hit_array:=bitmap.opaque_to_polygons(Rect2(Vector2(), bitmap.get_size()))
 	for i in hit_array:
 		var p=CollisionPolygon2D.new()
 		p.polygon=i
-		p.position.x-=texture.get_width()/2
-		p.position.y-=texture.get_height()/2
+		print(image.get_width()/2)
+		p.position.x-=image.get_width()/2
+		p.position.y-=image.get_height()/2
 		add_child(p)
-
+	$ActionPrompt._set_action("clean")
+	timer.timeout.connect(_on_cleaned)
+	color=sprite.modulate
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+func _unhandled_input(event):
+	if event.is_action_pressed("clean") && has_overlapping_bodies():
+		$ActionPrompt/ProgressBar.show()
+		cleaning=true
+		timer.start(3)
+		tween=get_tree().create_tween()
+		tween2=get_tree().create_tween()
+		var color_2=color
+		color_2.a=0
+		tween.tween_property(sprite, "modulate", color_2, 3)
+		tween2.tween_property($ActionPrompt/ProgressBar, "value", 100, 3)
+	if event.is_action_released("clean"):
+		$ActionPrompt/ProgressBar.hide()
+		cleaning=false
+		timer.stop()
+		tween.stop()
+		tween2.stop()
+		$ActionPrompt/ProgressBar.value=0
+		sprite.modulate=color
+		
+	
+
+func _on_body_entered(body):
+	$ActionPrompt.show()
+	pass # Replace with function body.
+
+
+func _on_body_exited(body):
+	$ActionPrompt.hide()
+	pass # Replace with function body.
+
+func _on_cleaned():
+	queue_free()
